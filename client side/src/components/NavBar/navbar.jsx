940fe539a9d34020c2b527fa6a98ce9132dcac31
EyalChachmi <125581744+EyalChachmi@users.noreply.github.com>
2024-08-13 15:58:17 +0300

    Login and Sign up Logic Completed Css W.I.P

--- client side/src/components/NavBar/navbar.jsx
+++ client side/src/components/NavBar/navbar.jsx
@@ -1,32 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 import SearchIcon from "@mui/icons-material/Search";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
-import "./navbar.css"
+import "./navbar.css";
 import { Link } from "react-router-dom";
-import img from "../../savedImages/israel.png"
-import { useState } from 'react';
+import img from "../../savedImages/israel.png";
 import Cart from '../Cart/cart';
-import {useSelector} from "react-redux";
-
-
-
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const [userEmail, setUserEmail] = useState(null);
+  const products = useSelector((state) => state.cart.products);
 
+  useEffect(() => {
+    // Function to fetch user email from localStorage
+    const fetchUserEmail = () => {
+      const email = localStorage.getItem("user");
+      setUserEmail(email);
+    };
 
-  const [open,setOpen]= useState(false)
-  const products = useSelector((state)=>state.cart.products);
+    // Fetch user email when the component mounts
+    fetchUserEmail();
 
+    // Poll for changes in localStorage every 500 milliseconds
+    const interval = setInterval(() => {
+      fetchUserEmail();
+    }, 500);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="navbarlocal">
       <div className="wrapper">
         <div className="left">
           <div className="item">
-            <img class="flag_of_israel"src={img} />
+            <img className="flag_of_israel" src={img} alt="Flag of Israel" />
             <KeyboardDoubleArrowDownIcon />
           </div>
           <div className="item">
@@ -34,31 +47,40 @@ const Navbar = () => {
             <KeyboardDoubleArrowDownIcon />
           </div>
           <div className="item">
-            <Link className ="link" to="/products/1">Our Products</Link>
+            <Link className="link" to="/products/1">Our Products</Link>
           </div>
         </div>
         <div className="center">
-          <Link className ="link" to="/">מותגים זה אנחנו</Link>
+          <Link className="link" to="/">מותגים זה אנחנו</Link>
         </div>
         <div className="right">
-          <div className="item">
-            <Link className ="link" to="/">Home</Link>
-          </div>
+          {userEmail ? (
+            <div className="item">
+              <span>Welcome, {userEmail}</span>
+            </div>
+          ) : (
+            <div className="item">
+              <Link className="link" to="/">Home</Link>
+            </div>
+          )}
           <div className="icons">
-            <Link className = "link" to= "/products/1">  
-              <SearchIcon/>
+            <Link className="link" to="/products/1">
+              <SearchIcon />
             </Link>
-            <PersonOutlineOutlinedIcon/>
-            <FavoriteBorderOutlinedIcon/>
-            <span className='cart' onClick={()=>setOpen(!open)}>
-              <ShoppingCartOutlinedIcon/>
+            <Link className="link" to="/login">
+              <PersonOutlineOutlinedIcon />
+            </Link>
+            <FavoriteBorderOutlinedIcon />
+            <span className="cart" onClick={() => setOpen(!open)}>
+              <ShoppingCartOutlinedIcon />
               <span>{products.length}</span>
             </span>
           </div>
         </div>
       </div>
-      {open && <Cart/>}
+      {open && <Cart />}
     </div>
   );
 };
-export default Navbar
+
+export default Navbar;
